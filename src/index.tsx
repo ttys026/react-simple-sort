55efe6671cf487b760116aab99f7d6add9d9191b
litianyi.troy <litianyi.troy@bytedance.com>
2023-12-27 12:29:26 +0800

    fix: dragging shadow style

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from 'react';
 
 interface Props {
   children: React.ReactNode;
   onSortEnd?: (oldIndex: number, newIndex: number) => void;
-  direction?: "vertical" | "horizontal";
+  direction?: 'vertical' | 'horizontal';
 }
 
 const createClone = (element: HTMLElement) => {
+  const styles = getComputedStyle(element);
   const clone = element.cloneNode(true) as HTMLElement;
-  clone.style.position = "absolute";
+  Array.from(styles).forEach((key) =>
+    clone.style.setProperty(key, styles.getPropertyValue(key), styles.getPropertyPriority(key))
+  );
   document.body.appendChild(clone);
   return {
     element: clone,
@@ -16,18 +19,14 @@ const createClone = (element: HTMLElement) => {
   };
 };
 
-const setDraggingShadow = (params: {
-  origin: HTMLElement;
-  clone: HTMLElement;
-  e: React.DragEvent<HTMLDivElement>;
-}) => {
+const setDraggingShadow = (params: { origin: HTMLElement; clone: HTMLElement; e: React.DragEvent<HTMLDivElement> }) => {
   const { origin, clone, e } = params;
   const { left, top } = origin.getBoundingClientRect();
   e.dataTransfer.setDragImage(clone, e.clientX - left, e.clientY - top);
 };
 
 export const Sortable: React.FC<Props> = (props) => {
-  const { direction = "vertical" } = props;
+  const { direction = 'vertical' } = props;
 
   const draggingIndex = useRef(-1);
   const draggingEle = useRef<HTMLDivElement | null>(null);
@@ -35,31 +34,18 @@ export const Sortable: React.FC<Props> = (props) => {
   const lastEnterRect = useRef<DOMRect | null>(null);
 
   useEffect(() => {
-    document.addEventListener("dragover", (e) => {
-      if (
-        !draggingEle.current ||
-        !lastEnterElement.current ||
-        !lastEnterRect.current ||
-        draggingIndex.current === -1
-      ) {
+    document.addEventListener('dragover', (e) => {
+      if (!draggingEle.current || !lastEnterElement.current || !lastEnterRect.current || draggingIndex.current === -1) {
         return;
       }
       e.preventDefault();
-      const getMousePosition = () =>
-        direction === "horizontal" ? e.clientX : e.clientY;
+      const getMousePosition = () => (direction === 'horizontal' ? e.clientX : e.clientY);
       const { left, width, top, height } = lastEnterRect.current;
-      const getElementPos = () =>
-        direction === "horizontal" ? left + width / 2 : top + height / 2;
+      const getElementPos = () => (direction === 'horizontal' ? left + width / 2 : top + height / 2);
       if (getMousePosition() > getElementPos()) {
-        lastEnterElement.current.insertAdjacentElement(
-          "afterend",
-          draggingEle.current!
-        );
+        lastEnterElement.current.insertAdjacentElement('afterend', draggingEle.current!);
       } else {
-        lastEnterElement.current.insertAdjacentElement(
-          "beforebegin",
-          draggingEle.current!
-        );
+        lastEnterElement.current.insertAdjacentElement('beforebegin', draggingEle.current!);
       }
     });
   }, []);
@@ -73,7 +59,7 @@ export const Sortable: React.FC<Props> = (props) => {
             draggable: true,
             onDragStart: (e: React.DragEvent<HTMLDivElement>) => {
               draggingIndex.current = index;
-              draggingEle.current = e.target as HTMLDivElement;
+              draggingEle.current = e.currentTarget as HTMLDivElement;
               const clone = createClone(draggingEle.current);
               setDraggingShadow({
                 origin: draggingEle.current,
@@ -81,17 +67,14 @@ export const Sortable: React.FC<Props> = (props) => {
                 e,
               });
               requestAnimationFrame(clone.destroy);
-              draggingEle.current.style.opacity = "0";
+              draggingEle.current.style.opacity = '0';
             },
             onDragEnd: () => {
               if (draggingEle.current) {
                 const parent = draggingEle.current.parentElement;
-                draggingEle.current.style.opacity = "";
+                draggingEle.current.style.opacity = '';
                 if (parent) {
-                  const newIndex = Array.prototype.indexOf.call(
-                    parent.children,
-                    draggingEle.current
-                  );
+                  const newIndex = Array.prototype.indexOf.call(parent.children, draggingEle.current);
                   props.onSortEnd?.(draggingIndex.current, newIndex);
                 }
               }
@@ -99,7 +82,7 @@ export const Sortable: React.FC<Props> = (props) => {
               draggingIndex.current = -1;
             },
             onDragEnter: (e: React.DragEvent<HTMLDivElement>) => {
-              const target = e.target as HTMLDivElement;
+              const target = e.currentTarget as HTMLDivElement;
               lastEnterElement.current = target;
               lastEnterRect.current = target.getBoundingClientRect();
             },
